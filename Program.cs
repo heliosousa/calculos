50381b7b32e6e8d978ad7a5e7baa007cfc9dda05
heliosousa <lousasr6@gmail.com>
2025-04-08 00:41:39 +0100

    Adicionar uma instanciação da classe conversoes

--- Program.cs
+++ Program.cs
@@ -10,10 +10,21 @@ namespace Calculos
     {
         static void Main(string[] args)
         {
+            // Criando uma instância da classe Conversoes
+            Conversoes conversoes = new Conversoes(); // instanciar a classe conversoes
+
             Console.WriteLine("Aplicação Cálculos");
-            Console.ReadKey();
+            //Console.ReadKey();
+            //Console.WriteLine("Pressione qualquer tecla para iniciar");
+
             Console.WriteLine($"4 + 2 = {Aritmetica.Somar(4, 2)}");
             Console.WriteLine($"4 - 2 = {Aritmetica.Subtrair(4, 2)}");
+            double t1 = conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
+            double t2 = conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsius, 100);
+
+            Console.WriteLine($"36º Celsius = {t1}º Fahrenheit");
+            Console.WriteLine($"100º Fahrenheit = {t2}º Celsius");
+
         }
     }
 }
